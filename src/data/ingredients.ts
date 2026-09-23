d14e8a7c3303f92a9f714c99cbf6bc8cc21664ba
roro78 <romerix.oum@gmail.com>
2026-09-23 19:30:24 +0200

    perf: switch ingredient imagery to local WebP

--- src/data/ingredients.ts
+++ src/data/ingredients.ts
@@ -10,6 +10,7 @@ export interface IngredientGuide {
   imageAlt: string;
   imageWidth: number;
   imageHeight: number;
+  imageWidths: number[];
 }
 
 export const ingredientGuides: IngredientGuide[] = [
@@ -21,10 +22,11 @@ export const ingredientGuides: IngredientGuide[] = [
     profile: ['Citronnelle', 'Galanga', 'Curcuma', 'Combava', 'Ail et échalote'],
     uses: ['Amok', 'Soupes et currys', 'Marinades', 'Grillades'],
     tips: ['Émincer finement avant de piler.', 'Travailler les ingrédients les plus fibreux en premier.', 'Éviter d’ajouter trop de liquide au mixeur.'],
-    image: 'https://commons.wikimedia.org/wiki/Special:FilePath/Cha_kroeung.jpg?width=1600',
+    image: '/images/cuisine/kroeung-1600.webp',
     imageAlt: 'Plat cambodgien parfumé au kroeung',
     imageWidth: 5184,
-    imageHeight: 3456
+    imageHeight: 3456,
+    imageWidths: [480, 800, 1200, 1600]
   },
   {
     slug: 'poivre-de-kampot',
@@ -34,10 +36,12 @@ export const ingredientGuides: IngredientGuide[] = [
     profile: ['Noir : puissant et chaud', 'Rouge : plus mûr et fruité', 'Blanc : fin et direct', 'Vert frais : végétal et vif'],
     uses: ['Lok lak', 'Crabe et fruits de mer', 'Sauces au citron vert', 'Viandes grillées'],
     tips: ['Moudre au dernier moment.', 'Éviter une cuisson longue pour préserver les arômes.', 'Comparer plusieurs maturités sur un même plat.'],
-    image: 'https://commons.wikimedia.org/wiki/Special:FilePath/Kampot_Pepper.jpg?width=1600',
+    image: '/images/cuisine/kampot-1600.webp',
     imageAlt: 'Grappes de poivre vert de Kampot sur le plant',
     imageWidth: 6000,
-    imageHeight: 4000
+    imageHeight: 4000,
+    imageWidths: [480, 800, 1200, 1600],
+    imageWidths: [480, 800, 1200, 1600]
   },
   {
     slug: 'tamarin',
@@ -47,10 +51,11 @@ export const ingredientGuides: IngredientGuide[] = [
     profile: ['Acidulé', 'Fruité', 'Légèrement sucré'],
     uses: ['Soupes', 'Sauces', 'Poissons', 'Préparations aigres-douces'],
     tips: ['Diluer la pulpe puis filtrer.', 'Ajouter progressivement et goûter.', 'Équilibrer avec sucre, sel et umami.'],
-    image: 'https://commons.wikimedia.org/wiki/Special:FilePath/Tamarind_fruit_(Tamarindus_indica).jpg?width=1600',
+    image: '/images/cuisine/tamarind-1600.webp',
     imageAlt: 'Gousses de tamarin ouvertes et entières',
     imageWidth: 2304,
-    imageHeight: 1268
+    imageHeight: 1268,
+    imageWidths: [480, 800, 1200, 1600]
   },
   {
     slug: 'prahok',
@@ -60,7 +65,7 @@ export const ingredientGuides: IngredientGuide[] = [
     profile: ['Salin', 'Fermenté', 'Très umami'],
     uses: ['Sauces et dips', 'Soupes', 'Plats mijotés', 'Assaisonnement de préparations traditionnelles'],
     tips: ['Commencer par une petite quantité.', 'Tenir compte de sa salinité avant d’ajouter d’autres sauces.', 'Choisir un produit adapté à l’usage prévu.'],
-    image: 'https://commons.wikimedia.org/wiki/Special:FilePath/Prahok_ktis.jpg?width=1600',
+    image: '/images/cuisine/prahok-1600.webp',
     imageAlt: 'Prahok ktis cambodgien servi avec des légumes',
     imageWidth: 3000,
     imageHeight: 4000
